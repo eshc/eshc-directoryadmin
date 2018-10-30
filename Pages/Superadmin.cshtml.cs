2dffd36a4639071b52967a4ad26eca1435372847
Jakub Szewczyk <kubasz51@gmail.com>
2018-10-30 21:46:21 +0000

    Permission delegation submitting

--- Pages/Superadmin.cshtml.cs
+++ Pages/Superadmin.cshtml.cs
@@ -40,16 +40,6 @@ namespace eshc_diradmin.Pages
             AllInfo = Startup.ldap.FetchAllMembersInfo();
             AllGroups = Startup.ldap.FetchAllGroups();
             newDetails = new NewDetails();
-            newDetails.membershipMatrix = new List<List<bool>>(AllGroups.Count);
-            foreach (var grp in AllGroups)
-            {
-                List<bool> users = new List<bool>(AllInfo.Count);
-                foreach (var user in AllInfo)
-                {
-                    users.Add(user.Groups.Contains(grp.Key));
-                }
-                newDetails.membershipMatrix.Add(users);
-            }
             return Page();
         }
 
@@ -71,16 +61,65 @@ namespace eshc_diradmin.Pages
                 return Redirect("/Login");
             }
             AllInfo = Startup.ldap.FetchAllMembersInfo();
+            AllGroups = Startup.ldap.FetchAllGroups();
             // update details
+            if (newDetails.membershipMatrix.Count != AllGroups.Count)
+            {
+                ModelState.AddModelError("", "Group count has changed");
+            }
+            if (newDetails.membershipMatrix.FirstOrDefault().Count != AllInfo.Count)
+            {
+                ModelState.AddModelError("", "Member count has changed");
+            }
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-            return RedirectToPage("Index");
+
+            for (int i = 0; i < AllGroups.Count; i++)
+            {
+                string GroupDN = AllGroups.ElementAt(i).Key;
+                LdapEntry GroupE = Startup.ldap.Connection.Read(GroupDN);
+                var Members = new List<string>();
+                for (int m = 0; m < AllInfo.Count; m++)
+                {
+                    if (newDetails.membershipMatrix[i][m])
+                    {
+                        Members.Add(AllInfo[m].DN);
+                    }
+                }
+                LdapModification Mod = new LdapModification(LdapModification.Replace, new LdapAttribute("member", Members.ToArray()));
+                try
+                {
+                    Startup.ldap.Connection.Modify(GroupDN, Mod);
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Failed to update group " + GroupDN);
+                }
+            }
+
+            return Page();
         }
 
         public class NewDetails
         {
+            public NewDetails()
+            {
+                var AllInfo = Startup.ldap.FetchAllMembersInfo();
+                var AllGroups = Startup.ldap.FetchAllGroups();
+                membershipMatrix = new List<List<bool>>(AllGroups.Count);
+                foreach (var grp in AllGroups)
+                {
+                    List<bool> users = new List<bool>(AllInfo.Count);
+                    foreach (var user in AllInfo)
+                    {
+                        users.Add(user.Groups.Contains(grp.Key));
+                    }
+                    membershipMatrix.Add(users);
+                }
+            }
+
             // [group][user]
             [Required]
             public List<List<bool>> membershipMatrix { get; set; }
